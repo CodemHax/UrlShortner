3e2be8b685830f97c6f9cf2ab3dd1310655d838c
codem <altaf.badure.it@ghrcem.raisoni.net>
2025-08-21 16:11:14 +0530

    Update config.py

--- config.py
+++ config.py
@@ -4,6 +4,6 @@ load_dotenv()
 
 class Config:
     mongo_uri = os.getenv("MONGO_URI")
-    host = os.getenv("HOST", "localhost")
-    port = int(os.getenv("PORT", 3000))
+    host = os.getenv("HOST")
+    port = int(os.getenv("PORT"))
 
